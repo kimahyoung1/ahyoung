f026b03d17dec578d3c65fe76e3e30097e25b552
김아영 <dkdud532@naver.com>
2024-09-23 17:17:08 +0900

    9월23일 스프링 수업

---
+++ proj7_spring/src/main/java/kr/or/human/HomeController.java
@@ -0,0 +1,45 @@
+package kr.or.human;
+ 
+import java.text.DateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ * Handles requests for the application home page.
+ */
+@Controller
+// 컨트롤러 역할을 하는 컨포넌트 (requestMapping) 가질 수 있음
+//	클래스를 bean으로 등록 
+public class HomeController {
+	
+	// 필드
+	// static final : 상수
+	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
+	
+	/**
+	 * Simply selects the home view to render by returning its name.
+	 */
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+//	주소가 get방식 말고는 못 들어오게 막아줘
+	public String home(Locale locale, Model model) {
+		logger.info("Welcome home! The client locale is {}.", locale);
+		
+		
+		Date date = new Date();
+		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
+		
+		String formattedDate = dateFormat.format(date);
+		
+		model.addAttribute("serverTime", formattedDate );
+		// home.jsp에서 보내줌
+		return "home";
+	}
+	
+}
